e7523802591f249dc2ad790724740716a842c603
wayfar3 <daongocthang.vn@gmail.com>
2022-06-23 07:04:28 +0700

    debugging

--- Assets/Scripts/Abilities.cs
+++ Assets/Scripts/Abilities.cs
@@ -8,17 +8,39 @@ public class Abilities : MonoBehaviour
     [SerializeField] private KeyCode ability1;
     private bool _isCooldown1;
 
+    //Ability 1 Input Variables
+    Vector3 position;
+    public Canvas ability1Canvas;
+    public Image skillshot;
+    public Transform player;
+
     [Header("Ability 2")] [SerializeField] private Image abilityImage2;
     [SerializeField] private float cooldown2 = 10;
     [SerializeField] private KeyCode ability2;
     private bool _isCooldown2;
 
+    //Ability 2 Input Variables
+    public Image targetCircle;
+    public Image indicatorRangeCircle;
+    public Canvas ability2Canvas;
+    private Vector3 posUp;
+    public float maxAbility2Distance;
+
+    [Header("Ability 3")] public Image abilityImage3;
+    public float cooldown3 = 7;
+    bool isCooldown3 = false;
+    public KeyCode ability3;
 
     // Start is called before the first frame update
     void Start()
     {
         abilityImage1.fillAmount = 0;
         abilityImage2.fillAmount = 0;
+        abilityImage3.fillAmount = 0;
+
+        skillshot.GetComponent<Image>().enabled = false;
+        targetCircle.GetComponent<Image>().enabled = false;
+        indicatorRangeCircle.GetComponent<Image>().enabled = false;
     }
 
     // Update is called once per frame
@@ -26,31 +48,52 @@ public class Abilities : MonoBehaviour
     {
         Ability1();
         Ability2();
-    }
+        Ability3();
 
-    private void Ability2()
-    {
-        if (Input.GetKey(ability2) && !_isCooldown2)
+        RaycastHit hit;
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+        //Ability 1 Inputs
+        if (Physics.Raycast(ray, out hit, Mathf.Infinity))
         {
-            _isCooldown2 = true;
-            abilityImage2.fillAmount = 1;
+            position = new Vector3(hit.point.x, hit.point.y, hit.point.z);
         }
 
-        if (_isCooldown2)
+        //Ability 2 Inputs
+        if (Physics.Raycast(ray, out hit, Mathf.Infinity))
         {
-            abilityImage2.fillAmount -= 1 / cooldown2 * Time.deltaTime;
-
-            if (abilityImage2.fillAmount <= 0)
+            if (hit.collider.gameObject != this.gameObject)
             {
-                abilityImage2.fillAmount = 0;
-                _isCooldown2 = false;
+                posUp = new Vector3(hit.point.x, 10f, hit.point.z);
+                position = hit.point;
             }
         }
+
+        //Ability 1 Canvas Inputs
+        Quaternion transRot = Quaternion.LookRotation(position - player.transform.position);
+        ability1Canvas.transform.rotation = Quaternion.Lerp(transRot, ability1Canvas.transform.rotation, 0f);
+
+        //Ability 2 Canvas Inputs
+        var hitPosDir = (hit.point - transform.position).normalized;
+        float distance = Vector3.Distance(hit.point, transform.position);
+        distance = Mathf.Min(distance, maxAbility2Distance);
+
+        var newHitPos = transform.position + hitPosDir * distance;
+        ability2Canvas.transform.position = (newHitPos);
     }
 
     private void Ability1()
     {
         if (Input.GetKey(ability1) && !_isCooldown1)
+        {
+            skillshot.GetComponent<Image>().enabled = true;
+
+            //Disable Other UI
+            indicatorRangeCircle.GetComponent<Image>().enabled = false;
+            targetCircle.GetComponent<Image>().enabled = false;
+        }
+
+        if (skillshot.GetComponent<Image>().enabled == true && Input.GetMouseButtonDown(0))
         {
             _isCooldown1 = true;
             abilityImage1.fillAmount = 1;
@@ -59,11 +102,64 @@ public class Abilities : MonoBehaviour
         if (_isCooldown1)
         {
             abilityImage1.fillAmount -= 1 / cooldown1 * Time.deltaTime;
+            skillshot.GetComponent<Image>().enabled = false;
 
             if (abilityImage1.fillAmount <= 0)
             {
                 abilityImage1.fillAmount = 0;
                 _isCooldown1 = false;
+            }
+        }
+    }
+
+    private void Ability2()
+    {
+        if (Input.GetKey(ability2) && !_isCooldown2)
+        {
+            indicatorRangeCircle.GetComponent<Image>().enabled = true;
+            targetCircle.GetComponent<Image>().enabled = true;
+
+            //Disable Skillshot UI
+            skillshot.GetComponent<Image>().enabled = false;
+        }
+
+        if (targetCircle.GetComponent<Image>().enabled == true && Input.GetMouseButtonDown(0))
+        {
+            _isCooldown2 = true;
+            abilityImage2.fillAmount = 1;
+        }
+
+        if (_isCooldown2)
+        {
+            abilityImage2.fillAmount -= 1 / cooldown2 * Time.deltaTime;
+            
+            indicatorRangeCircle.GetComponent<Image>().enabled = false;
+            targetCircle.GetComponent<Image>().enabled = false;
+            
+            if (abilityImage2.fillAmount <= 0)
+            {
+                abilityImage2.fillAmount = 0;
+                _isCooldown2 = false;
+            }
+        }
+    }
+
+    void Ability3()
+    {
+        if (Input.GetKey(ability3) && isCooldown3 == false)
+        {
+            isCooldown3 = true;
+            abilityImage3.fillAmount = 1;
+        }
+
+        if (isCooldown3)
+        {
+            abilityImage3.fillAmount -= 1 / cooldown3 * Time.deltaTime;
+
+            if (abilityImage3.fillAmount <= 0)
+            {
+                abilityImage3.fillAmount = 0;
+                isCooldown3 = false;
             }
         }
     }
